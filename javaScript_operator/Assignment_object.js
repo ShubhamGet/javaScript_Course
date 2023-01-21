a39197026f21833aa2d3a7d08c510baa31f91879
Shubham Kumar Singh <102406929+ShubhamGet@users.noreply.github.com>
2023-01-21 17:41:58 +0530

    5th commit

---
+++ javaScript_operator/Assignment_object.js
@@ -0,0 +1,24 @@
+let x = 10;
+x += 5;
+
+console.log("The += operator: ", x);
+
+let a = 5;
+a -= 1;
+console.log("The -= operator: ", a);
+
+let b = 10;
+b *= 5;
+console.log("The *= operator: ", b);
+
+let c = 10;
+c /= 10;
+console.log("The /= operator: ", c);
+
+let d = 10;
+d %= 5;
+console.log("The %/ operator: ", d);
+
+let e = 10;
+e **= 5;
+console.log("The **/ operator: ", e);
--- javaScript_operator/js_operator.js
+++ javaScript_operator/js_operator.js
@@ -10,3 +10,6 @@ console.log("Div of two number is", num1 / num2);
 a = 10;
 b = 3;
 console.log("Moduls of two number is", a % b);
+
+console.log("Increment of number is ", a++);
+console.log("Decrement of number is ", --a);

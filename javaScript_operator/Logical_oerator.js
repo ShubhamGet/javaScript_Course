b8e6e3635d41b818210a75f94fea806a784a61ac
Shubham Kumar Singh <102406929+ShubhamGet@users.noreply.github.com>
2023-01-21 18:00:06 +0530

    6th

---
+++ javaScript_operator/Logical_oerator.js
@@ -0,0 +1,6 @@
+let a = 10;
+let b = 8;
+let c = 5;
+console.log("&& operator return the result true if both condtion are getting ture:: ", a > b && a > c);
+console.log("|| operator return true if any condtion getting true:: ", b > a || b > c);
+console.log(!(a > b));
---
+++ javaScript_operator/comparison_operator.js
@@ -0,0 +1,7 @@
+a = 10;
+console.log(a == 8);
+console.log(a != 8);
+console.log(a === 10);
+console.log(a !== 10);
+console.log(a > 8);
+console.log(a < 10);
